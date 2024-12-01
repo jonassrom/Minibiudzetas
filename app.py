bd4937981443782de045add3de53b15f2f1910f4
jonassrom <jonassrom@yahoo.com>
2024-12-01 12:36:16 +0200

    Sukurtas naujas failas
    Sukurtas meniu ir pradeta rasyti pasirinkimai
    Sukurtas quit ir pirmasis meniu pasirinkimas
    Sukurtas antrasis meniu pasirinkimas
    Pridetas meniu printas
    Sukurtas treciasis meniu pasirinkimas
    Sukurtas ketvirtasis meniu pasrinkimas
    Sukurtas penktasis meniu pasrininkimas
    Pridetas datos ivedimas(neveikia 5 pasirinkimas ValueErrom)
    Pakeistas datos ivedimas - sios dienos data(neveikia 5 pasirinkimas ValueErrom)
    Panaikintas datos ivedimas
    Pridėtas datetime prie pajamų ir išlaidų
    Pridėtos funkcijos išsaugoti į pickle failą ir užkrauti iš jo. Vis dar neveikia 5 pasirinkimas, dėl datos.

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
+# from funkcijos import issaugoti_duomenis, uzkrauti_duomenis
 import datetime
 import pickle
+import os
 
 pajamos = []
 islaidos = []
+
+def issaugoti_duomenis():
+    try:
+        with open('biudzetas.pkl', 'wb') as file:
+            # noinspection PyTypeChecker
+            pickle.dump([pajamos, islaidos], file)
+        print("Duomenys išsaugoti sėkmingai.\n")
+    except Exception as e:
+        print(f"Klaida išsaugant duomenis: {e}")
+
+
+def uzkrauti_duomenis():
+    global pajamos, islaidos
+    if os.path.exists('biudzetas.pkl'):
+        try:
+            with open('biudzetas.pkl', 'rb') as file:
+                pajamos, islaidos = pickle.load(file)
+            print("Duomenys užkrauti sėkmingai.\n")
+        except Exception as e:
+            print(f"Klaida užkraunant duomenis: {e}")
+    else:
+        print("Nėra rastas duomenų failas.\n")
 
 while True:
     print("1. Įvesti pajamas\n"
@@ -20,21 +44,24 @@ while True:
 
     elif pasirinkimas == "1":
         try:
-            # data = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            data = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             suma = float(input("Įveskite norimas pajamas > "))
             rusis = input("Įveskite pajamų rūšį > ")
-            pajamos.append((suma, rusis))
+            pajamos.append([data, suma, rusis])
             print("Pajamos įvestos!\n")
+            issaugoti_duomenis()
         except ValueError:
             print("Klaida, įveskite teisingą sumą. \n")
 
     elif pasirinkimas == "2":
         try:
-            # data = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            data = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             suma = float(input("Įveskite norimas išlaidas > "))
             rusis = input("Įveskite išlaidų rūšį > ")
-            islaidos.append((suma, rusis))
+            islaidos.append([data, suma, rusis])
             print("Išlaidos įvestos!\n")
+            issaugoti_duomenis()
         except ValueError:
             print("Klaida, įveskite teisingą sumą. \n")
 
@@ -43,8 +70,9 @@ while True:
             print("Pajamos dar neįvestos")
         else:
             print("\n ---- Pajamų sąrašas ----")
-            for suma, rusis in pajamos:
-                print(f"{rusis}: {suma} EUR")
+            for data, suma, rusis in pajamos:
+                print(f"<{data}> {rusis}: {suma} EUR")
+
             print()
 
     elif pasirinkimas == "4":
@@ -52,26 +80,22 @@ while True:
             print("Išlaidos dar nepridėtos")
         else:
             print("\n ---- Išlaidų sarašas ----")
-            for suma, rusis in islaidos:
-                print(f"{rusis}: {suma} EUR")
+            for data, suma, rusis in islaidos:
+                print(f"<{data}> {rusis}: {suma} EUR")
             print()
 
     elif pasirinkimas == "5":
-        pajamu_suma = sum([suma for suma, _ in pajamos])
-        islaidu_suma = sum([suma for suma, _ in islaidos])
-        balansas = pajamu_suma - islaidu_suma
-        print("\n ---- Statistika ----")
-        print(f"Pajamos: {pajamu_suma} EUR")
-        print(f"Išlaidos: {islaidu_suma} EUR")
-        print(f"Balansas: {balansas} EUR \n")
-        print()
+            pajamu_suma = sum([suma for suma, _ in pajamos])
+            islaidu_suma = sum([suma for suma, _ in islaidos])
+            balansas = pajamu_suma - islaidu_suma
+            print("\n ---- Statistika ----")
+            print(f"Pajamos: {pajamu_suma} EUR")
+            print(f"Išlaidos: {islaidu_suma} EUR")
+            print(f"Balansas: {balansas} EUR \n")
 
 
-
-
-
-
-
+# if __name__ == "__main__":
+    uzkrauti_duomenis()
 
 
 
